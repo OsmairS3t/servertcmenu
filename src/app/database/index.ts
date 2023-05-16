c1c4df93fad752818e80b23ade20a5dd4eea9b78
Osmair <osmair144@gmail.com>
2023-05-16 16:16:07 -0300

    Up

--- src/app/database/index.ts
+++ src/app/database/index.ts
@@ -4,7 +4,11 @@ const mongoURI = process.env.MONGO_URI;
 
 export default function connectMontoDB(): void {
     if (mongoURI) {
-        mongoose.connect(mongoURI)
+        mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        }, console.log("Database is running."))
     } else {
         console.log("Failed to connect to MOngoDB")
     }
